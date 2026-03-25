44553ab2c2576929ea6a36d195bcef2cdf844eab
pavel <pavelBulletkiller22@gmail.com>
2026-03-25 20:28:20 +0500

    pasta

---
+++ resources/views/home.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>SaintPasta</title>
+</head>
+
+<body>
+    <header>
+        <div class="shapka">
+            <div class="logo">
+                <h1>
+                    <h1 class="fatty">SAINT P</h1>ASTA
+                </h1>
+            </div>
+            <ul>
+                <li><a href="#">О компании</a></li>
+                <li><a href="#">Паста короткая</a></li>
+                <li><a href="#">Паста длинная</a></li>
+                <li><a href="#">Доставка</a></li>
+                <li><a href="#">Контакты</a></li>
+            </ul>
+            <div class="num">
+                <div class="karamba">
+                    <img src="" alt="">
+                    <p>+799999981</p>
+                </div>
+                <p>Заказать звонок</p>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="main2">
+            <h2>Наша команда</h2>
+            <div class="spis">
+                <ul>
+                    <li><img src="" alt=""></li>
+                    <li><img src="" alt=""></li>
+                    <li><img src="" alt=""></li>
+                </ul>
+                <img src="" alt="">
+                <pre>Евгений Мальцев
+                    Бренд-шеф
+                </pre>
+                <pre>Безумно любит своё дело. Каждый день он замешивает
+                     свежее тесто и заготавливает лучшие начинки для
+                    создания той самой итальянской классики.
+
+                    Каждый понедельник Евгений представляет
+                    экспериментальную пасту недели, вкус которой не станет
+                    серийным. Следите за новостями, успейте попробовать.</pre>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="podval">
+            <div class="up">
+                <h1>ОСТАЛИСЬ ВОПРОСЫ?</h1>
+                <form action="">
+                    <h3>Заполните небольшую форму и наш менеджер <br>
+                        связется с вами в ближайшее время
+                    </h3>
+                    <input type="text">
+                    <input type="tel">
+                    <input type="email">
+                    <button type="submit">Отправить</button>
+                </form>
+            </div>
+            <div class="down">
+                <div class="upper">
+                    <div class="logodown">
+                        <h1>
+                            <h1 class="fattydown">SAINT P</h1>ASTA
+                        </h1>
+                    </div>
+                    <ul>
+                        <li><a href="#">О компании</a></li>
+                        <li><a href="#">Паста короткая</a></li>
+                        <li><a href="#">Паста длинная</a></li>
+                        <li><a href="#">Доставка</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                </div>
+                <div class="middle">
+                    <input type="text" name="" id="">
+                    <ul>
+                        <li><a href="#"><img src="" alt=""></a></li>
+                        <li><a href="#"><img src="" alt=""></a></li>
+                        <li><a href="#"><img src="" alt=""></a></li>
+                        <li><a href="#"><img src="" alt=""></a></li>
+                    </ul>
+                    <div class="tel">
+                        <img src="" alt="">
+                        <p>+799999981</p>
+                    </div>
+                    <button>Обратная связь</button>
+                </div>
+                <span></span>
+                <div class="downer">
+                    <p>© 2020 Universe, Все права защищены</p><img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
